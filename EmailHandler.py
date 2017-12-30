3a2e7dfc57e20dc78b94c3c02bf0709e667d8ad9
danielcahall <danielenricocahall@gmail.com>
2017-12-29 21:19:29 -0500

    Changes

--- EmailHandler.py
+++ EmailHandler.py
@@ -16,8 +16,8 @@ class EmailHandler:
          'Oliver': 'palumb48@students.rowan.edu',
          'Dimah': "derad6@rowan.edu",
          'Alena': 'alenagusevarus@gmail.com',
-         'Hassan': 'hfshaykh@uabmc.edu'}
- 
+         'Dr. Hassan': 'hfshaykh@uabmc.edu'}
+    body = ""
     
     def __init__(self):
         self.server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -31,10 +31,13 @@ class EmailHandler:
     
     def prepareMessage(self, subject, body):
         self.msg['Subject'] = subject
-        self.msg.attach(MIMEText(body, 'plain'))
+        self.body = body
         
     def sendMessage(self, recipient):
         self.msg['To'] = self.addressBook.get(recipient)
+        self.body = "Hello " + self.addressBook.get(recipient) + "\n\n" + self.body
+        self.body = "\n\n" + "Regards,\n Mr. HPC"
+        self.msg.attach(MIMEText(self.body, 'plain'))
         self.server.sendmail(self.addr, self.addressBook.get(recipient), self.msg.as_string())
     
     def finish(self):
