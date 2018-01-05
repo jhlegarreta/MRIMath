7cae225e23580f2e0db3deb5a1140c7ed4f8c7ce
danielcahall <danielenricocahall@gmail.com>
2018-01-04 19:31:53 -0500

    ...

--- DataHandler.py
+++ DataHandler.py
@@ -19,26 +19,21 @@ class DataHandler:
         segments = [[] for _ in range(8)]
         print('Reading images')
         for j in range(start,finish):
-            save_img = True
-            if j < 10:
-                directory = os.fsencode(training_directory + '/Patient_(00' + str(j)  + ')_data/')
-            elif j < 100:
-                directory = os.fsencode(training_directory + '/Patient_(0' + str(j)  + ')_data/')
-            else:
-                directory = os.fsencode(training_directory + '/Patient_(' + str(j)  + ')_data/')
-            for file in os.listdir(directory + b'/Original_Img_Data'):
-                img = self.get_im(directory+b'/Original_Img_Data/'+file)
-                height, width = img.shape
-                if height != 240 or width != 240:
-                    save_img = False
-                    return
-                X_train.append(img)
-            if(save_img):
+            if((j>0 and j < 107) or j > 130):
                 print('Reading Patient ' + str(j))
-                segment_directory = os.fsencode(directory + b'Segmented_Img_Data')
-                for dir in os.listdir(segment_directory):
-                    for file in os.listdir(segment_directory+b'/'+dir):
-                        ind = file[4:5]
-                        segments[int(ind.decode())-1].append(self.get_im(segment_directory+b'/'+dir+b'/'+file));
+                if j < 10:
+                    directory = os.fsencode(training_directory + '/Patient_(00' + str(j)  + ')_data/')
+                elif j < 100:
+                    directory = os.fsencode(training_directory + '/Patient_(0' + str(j)  + ')_data/')
+                else:
+                    directory = os.fsencode(training_directory + '/Patient_(' + str(j)  + ')_data/')
+                for file in os.listdir(directory + b'/Original_Img_Data'):
+                    img = self.get_im(directory+b'/Original_Img_Data/'+file)
+                    X_train.append(img)
+                    segment_directory = os.fsencode(directory + b'Segmented_Img_Data')
+                    for dir in os.listdir(segment_directory):
+                        for file in os.listdir(segment_directory+b'/'+dir):
+                            ind = file[4:5]
+                            segments[int(ind.decode())-1].append(self.get_im(segment_directory+b'/'+dir+b'/'+file));
 
         return X_train, segments
