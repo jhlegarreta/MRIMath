2eab329578012cec34d9c7453aac54fb7a91910c
danielcahall <danielenricocahall@gmail.com>
2018-01-06 10:01:53 -0500

    testing

--- ConvolutionalEncoder.py
+++ ConvolutionalEncoder.py
@@ -9,15 +9,19 @@ from keras.utils.training_utils import multi_gpu_model
 import tensorflow as tf
 from DataHandler import DataHandler
 from TimerModule import TimerModule
+from time import strftime
 
 now = datetime.now()
 date_string = now.strftime('%Y_%m_%d')
-   
+
+
+dataHandler = DataHandler()
+emailHandler = EmailHandler()
+timer = TimerModule()
+
 F = 3
 S = 2 
-W = 240
-H = 240
-input_img = Input(shape=(W, H, 1))  
+input_img = Input(shape=(dataHandler.W, dataHandler.H, 1))  
 
 x = Conv2D(120, (F, F), activation='relu', padding='same')(input_img)
 x = Conv2D(80, (F, F), activation='relu', padding='same')(x)
@@ -34,11 +38,10 @@ x = Conv2D(120, (F, F), activation='relu', padding='same')(x)
 decoded = Conv2D(1, (F, F), activation='relu', padding='same')(x)
 
 emailHandler = EmailHandler()
-dataHandler = DataHandler()
 timer = TimerModule()
 
-training, segments = dataHandler.load_data('/coe_data/MRIMath/MS_Research/Patient_Data_Images', 1, 4)
-testing, segments2 = dataHandler.load_data('/coe_data/MRIMath/MS_Research/Patient_Data_Images',151,152)
+training, segments = dataHandler.loadData('/coe_data/MRIMath/MS_Research/Patient_Data_Images', 1, 4)
+testing, segments2 = dataHandler.loadData('/coe_data/MRIMath/MS_Research/Patient_Data_Images',151,152)
 
 model_directory = "/coe_data/MRIMath/MS_Research/MRIMath/Models/" + date_string
 if not os.path.exists(model_directory):
@@ -49,18 +52,6 @@ num_epochs = 1
 segmentation_bank = [[] for _ in range(8)]
 for i in range(0,8):
     print('Training network: ' + str(i))
-    n_imgs = len(training)
-    training = np.array(training)
-    training =training.reshape(n_imgs,W,H,1)
-    training = training.astype('float32') / 255;
-    segments[i] = np.array(segments[i]);
-    segments[i] = segments[i].reshape(n_imgs,W,H,1)
-    n_imgs2 = len(testing)
-    testing = np.array(testing)
-    testing =testing.reshape(n_imgs2,W,H,1)
-    testing= testing.astype('float32') / 255;
-    segments2[i] = np.array(segments2[i]);
-    segments2[i] = segments2[i].reshape(n_imgs2,240,240,1)
     with tf.device('/cpu:0'):
         segmentation_bank[i] = Model(input_img, decoded)
     parallel_segmentation_bank = multi_gpu_model(segmentation_bank[i], G)
@@ -77,7 +68,7 @@ for i in range(0,8):
     segmentation_bank[i].save(model_directory + '/model_' + str(i) +'.h5')
     emailHandler.connectToServer()
     message = "Finished training network " + str(i) + " at " + str(datetime.now())
-    message += "\n Total training time: " + timer.getElapsedTime()
+    message += "\n Total training time: " + strftime(timer.getElapsedTime())
     message += "\n\n " + segmentation_bank[i].summary()
     emailHandler.prepareMessage("Network Training Finished!", message);
     emailHandler.sendMessage("Danny")
--- DataHandler.py
+++ DataHandler.py
@@ -6,15 +6,19 @@ Created on Jan 1, 2018
 
 import os
 import cv2
+import numpy as np
 
 class DataHandler:
+    
+    W = 240
+    H = 240
 
-    def get_im(self, path):
+    def getImage(self, path):
         path=path.decode()
         img = cv2.imread(path,0)
         return img
 
-    def load_data(self, training_directory, start, finish):
+    def loadData(self, training_directory, start, finish):
         X_train = []
         segments = [[] for _ in range(8)]
         print('Reading images')
@@ -28,12 +32,23 @@ class DataHandler:
                 else:
                     directory = os.fsencode(training_directory + '/Patient_(' + str(j)  + ')_data/')
                 for file in os.listdir(directory + b'/Original_Img_Data'):
-                    img = self.get_im(directory+b'/Original_Img_Data/'+file)
+                    img = self.getImage(directory+b'/Original_Img_Data/'+file)
                     X_train.append(img)
                 segment_directory = os.fsencode(directory + b'Segmented_Img_Data')
                 for dir in os.listdir(segment_directory):
                     for file in os.listdir(segment_directory+b'/'+dir):
                         ind = file[4:5]
                         segments[int(ind.decode())-1].append(self.get_im(segment_directory+b'/'+dir+b'/'+file));
-
-        return X_train, segments
+        training, segment_data = self.preprocessForNetwork(X_train, segments)
+        return training, segment_data
+    
+    def preprocessForNetwork(self, training_data, segment_data):
+        n_imgs = len(training_data)
+        training = np.array(training_data)
+        training =training.reshape(n_imgs,self.W,self.H,1)
+        training = training.astype('float32') / 255;
+        segments = np.array(segment_data);
+        segments = segments.reshape(n_imgs,self.W,self.H,1)
+        return training, segments
+        
+        
