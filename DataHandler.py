03ce1d6c2d5c75175668f7cba64af4d9c95e866a
danielcahall <danielenricocahall@gmail.com>
2018-01-07 15:28:10 -0500

    I think it's working yay

--- DataHandler.py
+++ DataHandler.py
@@ -45,6 +45,7 @@ class DataHandler:
             for file in os.listdir(directory + b'/Original_Img_Data'):
                 img = self.getImage(directory+b'/Original_Img_Data/'+file)
                 self.mri_images.append(img)
+                print("Appending!")
             segment_directory = os.fsencode(directory + b'Segmented_Img_Data')
             for dir in os.listdir(segment_directory):
                 for file in os.listdir(segment_directory+b'/'+dir):
