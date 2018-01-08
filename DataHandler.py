59791af9435120c378889e561023aabfbbe0df6f
danielcahall <danielenricocahall@gmail.com>
2018-01-07 20:55:31 -0500

    continuing...

--- DataHandler.py
+++ DataHandler.py
@@ -40,7 +40,7 @@ class DataHandler:
         print('Reading images')
         num_cores = multiprocessing.cpu_count()
         pool = ThreadPool(num_cores) 
-        for img, segment in pool.map(partial(self.loadIndividualImage, training_directory=training_directory, X_train=X_train, segment_data=segment_data), range(start, finish)):
+        for img, segment in pool.map(partial(self.loadIndividualImage, training_directory=training_directory, X_train=X_train, segment_data=segment_data), range(start, finish), chunksize=1):
             X_train.append(img)
             segment_data.append(segment)
         training, segments = self.preprocessForNetwork(X_train, segment_data)
