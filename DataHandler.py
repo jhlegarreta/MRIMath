5cefebf7550aea35957555cdb251cf33ee5de5fe
danielcahall <danielenricocahall@gmail.com>
2018-01-21 14:13:19 -0500

    simplifying code still ahhh help

--- DataHandler.py
+++ DataHandler.py
@@ -94,8 +94,8 @@ class DataHandler:
         self.X = np.array(self.X)
         self.X = self.X.reshape(n_imgs,self.n,self.n,1)
         self.X = self.X.astype('float32') / 255;
-        #self.labels = np.array(self.labels);
-        np_utils.to_categorical(self.labels, nb_classes=8)
+        self.labels = np.array(self.labels);
+        self.labels = np_utils.to_categorical(self.labels, nb_classes=8)
         #self.labels = np.array(self.labels);
         #self.labels = self.labels.reshape(n_imgs,8)
     
