2bcb6dcc6232d4af18a01bf2817a757469e3f120
danielcahall <danielenricocahall@gmail.com>
2018-02-19 20:04:02 -0500

    ...

--- DataHandler.py
+++ DataHandler.py
@@ -179,16 +179,19 @@ class DataHandler:
         if not os.path.exists(label_dir):
             return
         label_img = self.getImage(label_dir + file)
-        if np.sum(label_img) !=  0:
+        if np.count_nonzero(label_img) > 0.05*label_img.size:
             for _ in range(0,self.numPatches):
-                x,y,patch = self.extractPatch(label_img)
+                patch = np.zeros((self.n, self.n))
+                while np.count_nonzero(patch) < self.tolerance*patch.size:
+                    x,y,patch = self.extractPatch(label_img)
             ## if the entire image is background, we could be stuck in an infinite loop
             ## this mitigates that problem (presumably)
             ## Note to self: refactor this at some point
-                self.labels.append(int(patch[floor(self.n/2),floor(self.n/2)]/255))
-                self.X.append(region[x:x+self.n, y:y+self.n])
+                    self.labels.append(int(patch[floor(self.n/2),floor(self.n/2)]/255))
+                    self.X.append(region[x:x+self.n, y:y+self.n])
         #self.X.append(patch)
-        
+    
+
         #print(label_dir + patient_directory[len(patient_directory)-20:len(patient_directory)])
             
             
@@ -261,6 +264,6 @@ class DataHandler:
     def clearVectors(self):
         self.X =[]
         self.labels = []
-
-        
+    
+  
         
