c6acf94ed5d4113a39c886fd143df54658806f31
Danny <33044223+danielenricocahall@users.noreply.github.com>
2018-06-18 08:15:29 -0400

    Update config again
    More verbose

--- configure.sh
+++ configure.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
-python3 Utils/getBraTs2018Data.py
-
+echo "Pulling BRATS 2018 Data down, this may take a few minutes..."
+if command -v python3 &>/dev/null; then
+  python3 Utils/getBraTs2018Data.py
+else
+  python Utils/getBraTS2018Data.py
+fi
+echo "Data successfully pulled down! Now to pull down the Mask R-CNN submodule..." 
 git submodule update --init --recursive --remote
+echo "Success! You should be good to go! :)" 
