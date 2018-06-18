52d418ea27d98adf75ed4681aae28ce47b240861
Danny <33044223+danielenricocahall@users.noreply.github.com>
2018-06-18 09:14:04 -0400

    Update

--- Utils/setup.py
+++ Utils/setup.py
@@ -1,21 +1,14 @@
 import pip
-from HardwareHandler import HardwareHandler
 def import_or_install(package):
     try:
         __import__(package)
     except ImportError:
         pip.main(['install', package])  
 
-
-hardwareHandler = HardwareHandler()
-
 import_or_install("nibabel")
 import_or_install("requests")
 import_or_install("multiprocessing")
-if hardwareHandler.getAvailableGPUs() > 0:
-  import_or_install("tensorflow-gpu")
-else:
-  import_or_install("tensorflow")
+import_or_install("tensorflow")
 import_or_install("keras")
 import_or_install("opencv-python")
 import_or_install("matplotlib")
