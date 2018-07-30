dfd23adc312c328f7653b88e636a9c021d053a07
Danny <33044223+danielenricocahall@users.noreply.github.com>
2018-07-30 06:18:49 -0400

    Update ProbabilisticNMFComputer.py

--- NMFComputer/ProbabilisticNMFComputer.py
+++ NMFComputer/ProbabilisticNMFComputer.py
@@ -8,40 +8,45 @@ from NMFComputer.NMFComputer import NMFComputer
 import numpy as np
 from functools import partial
 import matplotlib.pyplot as plt
+    from numpy import linalg
 
 
 class ProbabilisticNMFComputer(NMFComputer):
     
-    def __init__(self, block_size = 400, num_hist_bins = 256, num_components = 5):
+    def __init__(self, block_size = 400, num_hist_bins = 256, num_components = 8):
         super().__init__(block_size, num_hist_bins, num_components)
+        
+        
+    def cost(A, W, H):
+        WH = np.matmul(W, H)
+        A_WH = A-WH
+        return linalg.norm(A_WH, 'fro')
+    
     
     def computeNMF(self, V):
+        
         sigma = np.var(V)
-        W = np.random.rand(V.shape[0], self.num_components)
-        H = np.random.rand(self.num_components, V.shape[1])
-        if np.count_nonzero(V) <= 10:
-            return W, H
+        
+        W = np.abs(np.random.uniform(low=0, high=1, size=(V.shape[0], self.num_components)))
+        H = np.abs(np.random.uniform(low=0, high=1, size=(self.num_components, V.shape[1])))
+     
         for _ in range(0, 10):
             lamda_H = sigma/np.var(H)
             lamda_W  = sigma/np.var(W)
 
-                
             H_norm = np.matmul(H, H.T)
             W_norm = np.matmul(W.T, W)
 
-                
-            W_mag = np.matmul(W.T, V)
-            H_mag = np.matmul(V, H.T)
+            WV = np.matmul(W.T, V)
+            HV = np.matmul(V, H.T)
 
-            prod_1 = np.matmul(W_norm, H)
-            prod_2 = np.matmul(W, H_norm)
+            WTWH = np.matmul(W_norm, H)
+            WHHT = np.matmul(W, H_norm)
             
-                        
+            H *= np.divide(WV,WTWH+lamda_H*H)
+            W *= np.divide(HV,WHHT+lamda_W*W)
             
-            H = np.multiply(H,np.divide(W_mag,np.add(prod_1,lamda_H*H)))
-            W = np.multiply(W,np.divide(H_mag,np.add(prod_2,lamda_W*W)))
-            
-            print(np.sum(np.subtract(V, np.matmul(W,H))))
+            print(self.cost(V, W, H))
             
             
             
